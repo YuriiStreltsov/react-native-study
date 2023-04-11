c4000cb2326962f71c38596945f18fa13a9333ee
Yurii Streltsov <streltsovyurii86@gmail.com>
2023-04-11 22:08:31 +0300

    Added styles Grid

--- App.js
+++ App.js
@@ -1,125 +1,16 @@
-import {
-    ImageBackground,
-    Platform,
-    SafeAreaView,
-    StyleSheet,
-} from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useCallback, useEffect, useState } from 'react';
-import GameScreen from './screens/GameScreen';
-import GameOverScreen from './screens/GameOverScreen';
+import { StyleSheet } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
-import Colors from './constants/colors';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
-
-SplashScreen.preventAutoHideAsync();
-
-let customFonts = {
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-};
 
 export default function App() {
-    const [appIsReady, setAppIsReady] = useState(false);
-    const [userNumber, setUserNumber] = useState();
-    const [gameIsOver, setGameIsOver] = useState(true);
-    const [guessRounds, setGuessRounds] = useState(0);
-
-    useEffect(() => {
-        async function prepare() {
-            try {
-                await Font.loadAsync(customFonts);
-            } catch (e) {
-                console.warn(e);
-            } finally {
-                setAppIsReady(true);
-            }
-        }
-
-        prepare();
-    }, []);
-
-    const onLayoutRootView = useCallback(async () => {
-        if (appIsReady) {
-            await SplashScreen.hideAsync();
-        }
-    }, [appIsReady]);
-
-    if (!appIsReady) {
-        return null;
-    }
-
-    function pickedNumberHandler(pickedNumber) {
-        setUserNumber(pickedNumber);
-        setGameIsOver(false);
-    }
-
-    function gameOverHandler(numberOfRounds) {
-        setGameIsOver(true);
-        setGuessRounds(numberOfRounds);
-    }
-
-    function startNewGameHandler() {
-        setUserNumber(null);
-        setGameIsOver(true);
-        setGuessRounds(0);
-    }
-
-    let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
-
-    if (userNumber) {
-        screen = (
-            <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
-        );
-    }
-
-    if (gameIsOver && userNumber) {
-        screen = (
-            <GameOverScreen
-                userNumber={userNumber}
-                roundsNumber={guessRounds}
-                onStartNewGame={startNewGameHandler}
-            />
-        );
-    }
-
     return (
         <>
             <StatusBar style="light" />
-            <LinearGradient
-                colors={[Colors.primary700, Colors.accent500]}
-                style={styles.rootScreen}
-            >
-                <ImageBackground
-                    source={require('./assets/images/background.png')}
-                    resizeMode="cover"
-                    style={styles.rootScreen}
-                    imageStyle={styles.backgroundImage}
-                >
-                    <SafeAreaView
-                        style={styles.safeAreaWrapper}
-                        onLayout={onLayoutRootView}
-                    >
-                        {screen}
-                    </SafeAreaView>
-                </ImageBackground>
-            </LinearGradient>
+            <CategoriesScreen />
         </>
     );
 }
 
 const styles = StyleSheet.create({
-    safeAreaWrapper: {
-        flex: 1,
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    },
-
-    rootScreen: {
-        flex: 1,
-    },
-    backgroundImage: {
-        opacity: 0.15,
-    },
+    container: {},
 });
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,25 @@
+import { CATEGORIES } from '../data/dummy-data';
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/ui/CategoryGridTile';
+
+function renderCategoryItem(itemData) {
+    return (
+        <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />
+    );
+}
+
+function CategoriesScreen() {
+    return (
+        <FlatList
+            data={CATEGORIES}
+            keyExtractor={(item) => item.id}
+            renderItem={renderCategoryItem}
+            numColumns={2}
+        />
+    );
+}
+
+export default CategoriesScreen;
