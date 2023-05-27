9f62f919596628d5128a6b67aa82224f979c81c3
YuriiStreltsov <streltsovyurii86@gmail.com>
2023-05-27 17:02:33 +0300

    Added starting navigation setup

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpenseOverview() {
     return (
-        <View style={styles.container}>
-            <Text>Hello world!!!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <BottomTabs.Navigator>
+            <BottomTabs.Screen
+                name="RecentExpenses"
+                component={RecentExpenses}
+            />
+            <BottomTabs.Screen name="AllExpenses" children={AllExpenses} />
+        </BottomTabs.Navigator>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+export default function App() {
+    return (
+        <>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="ExpensesOverview"
+                        component={ExpenseOverview}
+                    />
+                    <Stack.Screen
+                        name="ManageExpense"
+                        component={ManageExpense}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
+}
