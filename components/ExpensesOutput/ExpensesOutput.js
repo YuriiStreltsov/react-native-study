240d617eeafd66971c6a6784ef0624eae59d94b5
YuriiStreltsov <streltsovyurii86@gmail.com>
2023-05-28 21:38:42 +0300

    Added main components and them styles

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,59 @@
+import { StyleSheet, View } from 'react-native';
+import ExpensesSummary from './ExpensesSummary';
+import ExpensesList from './ExpensesList';
+import { GlobalStyles } from '../../constants/styles';
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 40.32,
+        date: new Date('2022-02-24'),
+    },
+    {
+        id: 'e2',
+        description: 'A pair of trousers',
+        amount: 12.99,
+        date: new Date('2022-09-01'),
+    },
+    {
+        id: 'e3',
+        description: 'Some bananas',
+        amount: 0.99,
+        date: new Date('2023-03-05'),
+    },
+    {
+        id: 'e4',
+        description: 'A book',
+        amount: 99.95,
+        date: new Date('2023-04-12'),
+    },
+    {
+        id: 'e5',
+        description: 'A car',
+        amount: 10000.0,
+        date: new Date('2023-05-05'),
+    },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+    return (
+        <View style={styles.container}>
+            <ExpensesSummary
+                expenses={DUMMY_EXPENSES}
+                periodName={expensesPeriod}
+            />
+            <ExpensesList expenses={DUMMY_EXPENSES} />
+        </View>
+    );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700,
+    },
+});
