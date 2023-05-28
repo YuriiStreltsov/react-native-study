6b996e2bf6617f3cc3a3d7fccc50e1faf4a71712
YuriiStreltsov <streltsovyurii86@gmail.com>
2023-05-28 22:07:43 +0300

    Styling expenses list

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -34,6 +34,37 @@ const DUMMY_EXPENSES = [
         amount: 10000.0,
         date: new Date('2023-05-05'),
     },
+
+    {
+        id: 'e6',
+        description: 'A pair of shoes',
+        amount: 40.32,
+        date: new Date('2022-02-24'),
+    },
+    {
+        id: 'e8',
+        description: 'A pair of trousers',
+        amount: 12.99,
+        date: new Date('2022-09-01'),
+    },
+    {
+        id: 'e9',
+        description: 'Some bananas',
+        amount: 0.99,
+        date: new Date('2023-03-05'),
+    },
+    {
+        id: 'e10',
+        description: 'A book',
+        amount: 99.95,
+        date: new Date('2023-04-12'),
+    },
+    {
+        id: 'e11',
+        description: 'A car',
+        amount: 10000.0,
+        date: new Date('2023-05-05'),
+    },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -53,7 +84,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 24,
+        paddingHorizontal: 24,
+        paddingTop: 24,
+        paddingBottom: 0,
         backgroundColor: GlobalStyles.colors.primary700,
     },
 });
