e2f3419347a75dedd7c10da8d46760b22c4ec306
YuriiStreltsov <streltsovyurii86@gmail.com>
2023-06-09 01:22:30 +0300

    start

---
+++ components/Places/PlaceItem.tsx
@@ -0,0 +1,23 @@
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Place } from './places-types';
+
+interface IPlaseItemProps {
+    place: Place;
+    onSelect: () => void;
+}
+
+function PlaceItem({ place, onSelect }: IPlaseItemProps): JSX.Element {
+    return (
+        <Pressable onPress={onSelect}>
+            <Image source={{ uri: place.imageUri }} />
+            <View>
+                <Text>{place.title}</Text>
+                <Text>{place.address}</Text>
+            </View>
+        </Pressable>
+    );
+}
+
+export default PlaceItem;
+
+const styles = StyleSheet.create({});
