41d12a61350133c0dfe817d31e4b6db3deaa6cdc
YuriiStreltsov <streltsovyurii86@gmail.com>
2023-06-11 23:46:19 +0300

    Added place form

--- components/Places/PlaceForm.tsx
+++ components/Places/PlaceForm.tsx
@@ -1,11 +1,43 @@
-import { Text, View } from 'react-native';
+import { useState } from "react";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { Colors } from "../../constants/colors";
 
 function PlaceForm() {
-    return (
-        <View>
-            <Text>Place Form</Text>
-        </View>
-    );
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText: string): void {
+    setEnteredTitle(enteredText);
+  }
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} onChangeText={changeTitleHandler} />
+      </View>
+    </ScrollView>
+  );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
