70da7e4cb5849612a76f5ca5d7e3085c2e5d8dad
YuriiStreltsov <streltsovyurii86@gmail.com>
2023-06-20 18:00:47 +0300

    Local Notifications

--- screens/AllPlaces.tsx
+++ screens/AllPlaces.tsx
@@ -3,6 +3,20 @@ import PlacesList from "../components/Places/PlacesList";
 import { useIsFocused } from "@react-navigation/native";
 import { Place } from "../model/place";
 import { fetchPlaces } from "../utils/database";
+import { StyleSheet, View } from "react-native";
+import Button from "../components/UI/Button";
+import { Colors } from "../constants/colors";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 function AllPlaces() {
   const [loadedPlaces, setLoadedPlaces] = useState<Place[]>([]);
@@ -20,7 +34,63 @@ function AllPlaces() {
     }
   }, [isFocused]);
 
-  return <PlacesList places={loadedPlaces} />;
+  // return <PlacesList places={loadedPlaces} />
+  return <CustomComponent />;
 }
 
 export default AllPlaces;
+
+function CustomComponent() {
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        const userName = notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        const userName = response.notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is body of the notification ",
+        data: { userName: "Yurii" },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={scheduleNotificationHandler}>
+        Schedule Notification
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
